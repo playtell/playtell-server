89770e1fc6120c027a99edc32a2efe81ab6b5b1e
semira rahemtulla <semira@gmail.com>
2011-02-15 15:48:05 -0800

    semiralabs begins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semiralabs::Application.config.secret_token = '928389a8bc867e1b986ea1d03aae4a039feafdbebc26a51824b0d8868a2046f42d403df5e69a993926d3e725aa05b59818c822931881fc088fcedae4adddf5c3'
