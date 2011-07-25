d522733d808a4455840a50cf208d4b6f164bcb28
semira <semira@gmail.com>
2011-07-25 15:27:52 -0700

    put images in s3

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,10 @@
 module GamesHelper  
   def getPageImageFilePath(title, pageNum)
-    File.join(title.parameterize, "page"+pageNum+".png")
+    #for local storage
+    #File.join(title.parameterize, "page"+pageNum+".png")
+    
+    #for S3 storage, e.g. https://ragatzi.s3.amazonaws.com/little-red-riding-hood-page1.png
+    "https://ragatzi.s3.amazonaws.com/" + title.parameterize + "-" + "page" + pageNum + ".png"
   end
   
   def fellowPlayers()
