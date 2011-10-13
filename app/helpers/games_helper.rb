0500683a403f49eb689aa3d96d8fbd049a9f6aad
semira <semira@gmail.com>
2011-10-13 12:30:42 -0700

    changed icons in toybox and added hover text for buttons

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,7 +4,15 @@ module GamesHelper
     #File.join(title.parameterize, "page"+pageNum+".png")
     
     #for S3 storage, e.g. https://ragatzi.s3.amazonaws.com/little-red-riding-hood-page1.png
-    "https://ragatzi.s3.amazonaws.com/" + directory + "-" + "page" + pageNum + ".png"
+    path = "https://ragatzi.s3.amazonaws.com/" + directory; 
+    if pageNum > 0
+      path += "-" + "page" + pageNum.to_s
+    end
+    path += ".png"
+  end
+  
+  def getImageFilePath(name)
+    "https://ragatzi.s3.amazonaws.com/" + name;
   end
   
   def fellowPlayers()
