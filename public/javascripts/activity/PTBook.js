4e7ac8f27f797c506a45ed2b8155676edb6ece3e
Semira Rahemtulla <semira@gmail.com>
2012-03-01 14:25:13 -0800

    playspace UI, take 1

--- public/javascripts/activity/PTBook.js
+++ public/javascripts/activity/PTBook.js
@@ -12,7 +12,20 @@ PTBook.prototype.createActivityFromJSON=function(book) {
 		if (pageNum > 1) {
 			displayType = "none";
 		}
-		bookMarkup += '<li style=display:' + displayType + '><div id="page_' + pageNum + '"> <div class="book-image"><img src="' + getPageImageFilePath(book.image_directory, i+1) + '"></div> <div class="book-text">' + book.pages[i].page_text + '</div></div></li>';
+		bookMarkup += '<li style=display:' + displayType + '>'
+		 	+ '<div id="page_' + pageNum + '">'
+			+ '<div class="inline-block">'
+			+ '<img src="' + getPageImageFilePath(book.image_directory, i+1) + '" class="book-image">'
+			+ '<div class="inline-block fl bottom-shadow">'
+			+ '<img src="/images/photo_frame_left.png">'
+			+ '</div>'
+			+ '<div class="inline-block fr bottom-shadow">'
+			+ '<img src="/images/photo_frame_right.png">'
+			+ '</div>'
+			+ '</div>'
+			+ '<div class="book-text">' 
+			+ book.pages[i].page_text 
+			+ '</div></div></li>';
 	});
 	$('#pages').html("<ul>" + bookMarkup + "</ul>");
 }
