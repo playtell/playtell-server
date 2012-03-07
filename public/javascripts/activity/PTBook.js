20fded6627636dcbacd1fa34249d53894f8c6d6f
Semira Rahemtulla <semira@gmail.com>
2012-03-07 08:59:57 -0800

    title page changes for book

--- public/javascripts/activity/PTBook.js
+++ public/javascripts/activity/PTBook.js
@@ -12,24 +12,31 @@ PTBook.prototype.createActivityFromJSON=function(book) {
 		if (pageNum > 1) {
 			displayType = "none";
 		}
-		bookMarkup += '<li style=display:' + displayType + '>'
-		 	+ '<div id="page_' + pageNum + '" class="page">'
-			+ '<div class="inline-block">'
-			+ '<img src="' + getPageImageFilePath(book.image_directory, i+1) + '" class="book-image">'
-			+ '<div class="inline-block fl bottom-shadow">'
-			+ '<img src="/images/photo_frame_left.png">'
-			+ '</div>'
-			+ '<div class="inline-block fr bottom-shadow">'
-			+ '<img src="/images/photo_frame_right.png">'
-			+ '</div>'
-			+ '</div>'
-			+ '<div class="book-text">' 
-			+ book.pages[i].page_text 
-			+ '</div></div>';
 		if (pageNum == 1) {
-			bookMarkup += '<div class="title-page"><img src="/images/book_title_cover.png"></div>';
+			bookMarkup += '<li style=display:' + displayType + '>'
+			 	+ '<div id="page_' + pageNum + '" class="page">'
+				+ '<div class="inline-block">'
+				+ '<img src="' + getPageImageFilePath(book.image_directory, i+1) + '" class="title-image">'
+				+ '</div>'
+				+ '</div>'
+				+ '<div class="title-page"><img src="/images/book_title_cover.png"></div></li>';
 		}
-		bookMarkup += '</li>';
+		else {
+			bookMarkup += '<li style=display:' + displayType + '>'
+			 	+ '<div id="page_' + pageNum + '" class="page">'
+				+ '<div class="inline-block">'
+				+ '<img src="' + getPageImageFilePath(book.image_directory, i+1) + '" class="book-image">'
+				+ '<div class="inline-block fl bottom-shadow">'
+				+ '<img src="/images/photo_frame_left.png">'
+				+ '</div>'
+				+ '<div class="inline-block fr bottom-shadow">'
+				+ '<img src="/images/photo_frame_right.png">'
+				+ '</div>'
+				+ '</div>'
+				+ '<div class="book-text">' 
+				+ book.pages[i].page_text 
+				+ '</div></div></li>';
+		}
 	});
 	$('#pages').html("<ul>" + bookMarkup + "</ul>");
 }
