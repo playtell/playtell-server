2a8e4b186f15ac28be458d42194c72cdbc365ddf
Semira Rahemtulla <semira@gmail.com>
2012-03-13 20:56:00 -0700

    only registers device token if there is one

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -5,7 +5,7 @@ class Api::TokensController  < ApplicationController
   def create
     username = params[:username]
     password = params[:password]
-    device_token = params[:device_token]
+    device_token = params[:device_token] if params[:device_token]
     
     if request.format != :json
         render :status=>406, :json=>{:message=>"The request must be json"}
@@ -32,13 +32,14 @@ class Api::TokensController  < ApplicationController
       logger.info("User #{username} failed signin, password \"#{password}\" is invalid")
       render :status=>401, :json=>{:message=>"Invalid username or passoword."} 
     else
-      d = DeviceToken.find_or_create_by_user_id({ :user_id => @user.id })
-      if d.token != device_token
-        d.token = device_token
-        d.save!
-        Urbanairship.register_device(params[:device_token])
+      if !device_token.blank?
+        d = DeviceToken.find_or_create_by_user_id({ :user_id => @user.id })
+        if d.token != device_token
+          d.token = device_token
+          d.save!
+          Urbanairship.register_device(params[:device_token])
+        end
       end
-      
       render :status=>200, :json=>{:token=>@user.authentication_token, :user_id=>@user.id} 
     end
   end
