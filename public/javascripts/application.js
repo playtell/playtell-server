e29e872e00c4ed11a7cb5916fd4486ca13a2595b
Semira Rahemtulla <semira@gmail.com>
2012-03-18 17:41:08 -0700

    5th try is a charm! that last commit actually worked. adding it to playdate html file and user profile page file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -46,31 +46,3 @@ $(function() {
 		}
   	});
 });
-
-function preventUglyScroll(node) {
-
-	//var node = this[0];
-
-	node.ontouchstart = function(event) {
-	    touchStart = event;
-	    frameStart = $(node).offset().top;
-	}
-
-	node.ontouchmove = function(event){
-
-	    // block all two finger gestures
-	    if(event.touches.length > 1) {
-	        event.preventDefault();
-	        return false;
-	    }
-
-	    event.preventDefault();
-    
-	    var yDiff     = event.pageY - touchStart.pageY;
-	    var newTop  = yDiff + frameStart;
-	    var hMin     = 460 - $(node).height();
-	    if(newTop <= 0 && newTop > hMin) {
-	        $(node).css('margin-top', newTop);
-	    }
-	}
-}
