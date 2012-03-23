2d19642fde16a753dee3d1fa3aab1c4cdbb587a6
Semira Rahemtulla <semira@gmail.com>
2012-03-22 19:03:16 -0700

    fixed problems with update settings api

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -8,12 +8,12 @@ class Api::SettingsController < ApplicationController
   end
 
   def update
-    @user = User.find(current_user.id)
+    @user = User.find_by_email(params[:user][:email])
     if @user.update_attributes(params[:user])
       # Sign in the user by passing validation in case his password changed
-      token = @user.reset_authentication_token!
+      @user.reset_authentication_token!
       sign_in @user, :bypass => true
-      render :status=>200, :json=>{:token=>token}
+      render :status=>200, :json=>{:token=>@user.authentication_token}
     else
       puts @user.errors.full_messages.as_json
       render :status=>401, :json=>{:errors=>@user.errors.full_messages.as_json, :keys=>@user.errors.keys.as_json}
