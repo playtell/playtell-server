54c91703fc0851f95265bb74bd14700bf5a13c62
Semira Rahemtulla <semira@gmail.com>
2012-03-23 15:24:29 -0700

    changed how i refer to the request param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,7 +49,7 @@ class ApplicationController < ActionController::Base
   def incoming
     response = Twilio::TwiML.build do |res|
       res.dial do |g|
-        g.client request.request_parameters.clientName
+        g.client request.request_parameters[:clientName]
       end
     end
     render :text => response
