d7f716a97debbc53327450cdc1249f892dd09f28
Semira Rahemtulla <semira@gmail.com>
2012-03-23 18:29:35 -0700

    fixed endpoint for getting a capability token for twilio. requires authenticated call

--- app/controllers/api/twilio_controller.rb
+++ app/controllers/api/twilio_controller.rb
@@ -1,7 +1,9 @@
-class TwilioController < ApplicationController
+class Api::TwilioController < ApplicationController
   
-  #respond_to :xml
-  
+  skip_before_filter :verify_authenticity_token
+  before_filter :authenticate_user!, :only => [:capability_token]
+  respond_to :json
+
   def incoming
     response = Twilio::TwiML.build do |res|
       res.dial do |g|
@@ -12,7 +14,18 @@ class TwilioController < ApplicationController
   end
   
   def capability_token
-    twilio_token
+    puts current_user.username
+    token = Twilio::CapabilityToken.create \
+              account_sid:    Twilio::ACCOUNT_SID,
+              auth_token:     Twilio::AUTH_TOKEN,
+              allow_incoming: twilio_name,
+              allow_outgoing: 'AP53bc8a48b98b4da0a5ba8c4b83f5bc69' 
+    render :json=>{:token=>token} 
+  end
+  
+private
+  def twilio_name
+    current_user.id.to_s
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,3 @@
 module ApplicationHelper
   
-  def twilio_token
-    #Twilio::CapabilityToken.create \
-     #   allow_outgoing: 'APabe7650f654fc34655fc81ae71caa3ff' #Twilio::ACCOUNT_SID,
-        #allow_incoming: current_user.email  
-    Twilio::CapabilityToken.create \
-      account_sid:    Twilio::ACCOUNT_SID,
-      auth_token:     Twilio::AUTH_TOKEN,
-      allow_incoming: twilio_name,
-      allow_outgoing: 'AP53bc8a48b98b4da0a5ba8c4b83f5bc69'
-  end
-  
-  def twilio_name
-    @current_user.id.to_s
-  end
-  
 end
