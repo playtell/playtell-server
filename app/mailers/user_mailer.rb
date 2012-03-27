86016f7a20327224fa1a19932a8e0df639b05c85
Semira Rahemtulla <semira@gmail.com>
2012-03-27 10:10:53 -0700

    changed out homepage image, did some layout tweaking on homepage. added emails for different users but havent tested or hooked them up yet

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,19 @@
 class UserMailer < ActionMailer::Base
   default :from => "semira@playtell.com"
   
-  def earlyuser_confirmation(earlyuser)
-      mail(:to => earlyuser.email, :subject => "Hello from PlayTell :)")
+  def earlyuser_welcome(earlyuser)
+      @user = earlyuser
+      mail(:to => @user.email, :subject => "Hello from PlayTell :)")
+  end
+  
+  def betauser_welcome(betauser)
+      @user = betauser
+      mail(:to => @user.email, :subject => "PlayTell Beta - quick iPad setup")
+  end
+  
+  def betainvitee_welcome(betauser, invitee)
+      @invitee = invitee
+      @inviter = betauser
+      mail(:to => @user.email, :subject => betauser.username + " got you early access to PlayTell :)")
   end
 end
