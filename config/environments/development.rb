521f0d3fa358184919969c6ac158bb9abc708b8e
Semira Rahemtulla <semira@gmail.com>
2012-03-28 14:12:49 -0700

    configed to not send emails from dev env, but pushing to test emails getting sent from staging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Semiralabs::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false #try to force sending in development 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -23,6 +25,5 @@ Semiralabs::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
