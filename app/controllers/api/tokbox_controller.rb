ea7005c0bc814424da3fd15a30ca08a4af39abf1
Semira Rahemtulla <semira@gmail.com>
2012-04-13 16:20:12 -0700

    tests from previous commit worked. this commit properly changes the push notification to use root_url so the response will match the request. the change to tokbox_controller was just my testing ground.

--- app/controllers/api/tokbox_controller.rb
+++ app/controllers/api/tokbox_controller.rb
@@ -14,8 +14,7 @@ class Api::TokboxController < ApplicationController
                    :initiator_id=>@playdate.player1_id,
                    :initiator_token=>@playdate.tokbox_initiator_token,
                    :playmate_id=>@playdate.player2_id,                   
-                   :playmate_token=>@playdate.tokbox_playmate_token, 
-                   :test=>root_url.to_s + 'playdate'} 
+                   :playmate_token=>@playdate.tokbox_playmate_token} 
     end
   end
 
