230a0229e4d68f0fb60210ce4a651323945a8e3c
Semira Rahemtulla <semira@gmail.com>
2012-04-17 11:13:13 -0700

    updating urban airship credentials for upgraded account

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,5 +1,5 @@
-Urbanairship.application_key = 'fxu9jz-8RWeQX00kVS-OGw' #'cyKbVnBdTAmF4fVAUsspCQ' 
-Urbanairship.application_secret = 'W_wkhtswRaWuIL6qWBE7Nw' #'hf6orl8OTqaESSatZHwfiA' 
-Urbanairship.master_secret = 'nCLhOtFGRAe-SwFgIXEEWA' #'vOntPXEuSNOC1np0jPbT-A'
+Urbanairship.application_key = 'jzSsHsdlToKrK7D2tOzDuA'  #'fxu9jz-8RWeQX00kVS-OGw' 
+Urbanairship.application_secret = 'Rwaek7P5QkuFLtiXOrU8KA' #'W_wkhtswRaWuIL6qWBE7Nw' 
+Urbanairship.master_secret = 'o68st_HBRCS9Ys7KcmQEBw' #'nCLhOtFGRAe-SwFgIXEEWA' 
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 25 # default
