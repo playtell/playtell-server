b943e2a1fd23cc217761511e397963971eddfb7b
Semira Rahemtulla <semira@gmail.com>
2012-04-18 13:18:53 -0700

    changed ua credentials to differ based on environment staging or prod

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,5 +1,5 @@
-Urbanairship.application_key = 'jzSsHsdlToKrK7D2tOzDuA'  #'fxu9jz-8RWeQX00kVS-OGw' 
-Urbanairship.application_secret = 'Rwaek7P5QkuFLtiXOrU8KA' #'W_wkhtswRaWuIL6qWBE7Nw' 
-Urbanairship.master_secret = 'o68st_HBRCS9Ys7KcmQEBw' #'nCLhOtFGRAe-SwFgIXEEWA' 
+Urbanairship.application_key = ENV['UA_KEY'] || 'fxu9jz-8RWeQX00kVS-OGw' #staging is default
+Urbanairship.application_secret = ENV['UA_SECRET'] || 'W_wkhtswRaWuIL6qWBE7Nw'
+Urbanairship.master_secret = ENV['UA_MASTER_SECRET'] || 'nCLhOtFGRAe-SwFgIXEEWA'
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 25 # default
