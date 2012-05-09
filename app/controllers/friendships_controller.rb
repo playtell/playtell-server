063ef31fb78cbe9dd4f9a49bc9fba009d8b13247
Semira Rahemtulla <semira@gmail.com>
2012-05-08 18:51:01 -0700

    prevent creation of friendships that already exist, contain users that dont exist, or with self

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,26 +2,33 @@ class FriendshipsController < ApplicationController
   
   #creates a friendship with the user that is currently logged in
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Friend added!"
-      redirect_to user_path current_user
-    else
-      flash[:error] = "Unable to add friend"
-      redirect_to user_path current_user
+    fID = params[:friend_id]
+    if fID != current_user.id && !Friendship.exists(fID, current_user.id)
+      @friendship = current_user.friendships.build(:friend_id => fID)
+      if @friendship.save
+        flash[:notice] = "Friend added!"
+        redirect_to user_path current_user
+        return
+      end
     end
+    flash[:notice] = "Unable to add friend"
+    redirect_to user_path current_user
   end
   
   #for admin
   def add
-    @friendship = Friendship.new(:friend_id => params[:friend_id], :user_id => params[:user_id])
-    if @friendship.save
-      flash[:notice] = "Friend added!"
-      redirect_to allofplaytellsusers_users_path
-    else
-      flash[:error] = "Unable to add friend"
-      redirect_to allofplaytellsusers_users_path
+    fID = params[:friend_id]
+    uID = params[:user_id]
+    if fID != uID && !Friendship.exists(fID, uID) && User.find(fID)
+      @friendship = Friendship.new(:friend_id => params[:friend_id], :user_id => params[:user_id])
+      if @friendship.save
+        flash[:notice] = "Friend added!"
+        redirect_to allofplaytellsusers_users_path
+        return
+      end
     end
+    flash[:notice] = "Unable to add friend"
+    redirect_to allofplaytellsusers_users_path
   end
   
   def destroy
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+  
+  def self.exists(id1, id2)
+    Friendship.find_by_user_id_and_friend_id(id1, id2) || Friendship.find_by_user_id_and_friend_id(id2, id1)
+  end
 end
