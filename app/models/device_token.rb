4f3df293cd9d3ae8564aef34f375bdd3ab90d943
Semira Rahemtulla <semira@gmail.com>
2012-05-11 13:40:19 -0700

    pusher offline stuff uncommented. setting a device token confirms the user. user appears ghosted in dialpad until confirmed

--- app/models/device_token.rb
+++ app/models/device_token.rb
@@ -1,3 +1,8 @@
 class DeviceToken < ActiveRecord::Base
   belongs_to :user
+  after_save :confirm_user
+  
+  def confirm_user
+    User.find(self.user_id).confirmed
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   #DONE_SECOND_PLAYDATE
 
   def set_defaults
-    self.status = WAITING_FOR_UDID if self.status.blank?
+    self.status = WAITING_FOR_DOWNLOAD if self.status.blank?
   end
 
   # auto-adds Test as this user's first friend
