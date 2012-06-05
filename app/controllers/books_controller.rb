2ecc200248ced7ca183067627bd23ad24b08652d
Dimitry <dimitryb@gmail.com>
2012-06-05 16:48:14 -0400

    Moved view, added layouts for books/pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_filter :authenticate_user!
+  layout 'books'
   
   def show
     @book = Book.find(params[:id])
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
+  layout 'pages'
   
   def show
     @book = Book.find(params[:book_id])
