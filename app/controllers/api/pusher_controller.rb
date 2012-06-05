00f15fd6e26492450cebf038c57243969ec72e12
Semira Rahemtulla <semira@gmail.com>
2012-06-05 08:19:46 -0700

    fixed check for disconnected

--- app/controllers/api/pusher_controller.rb
+++ app/controllers/api/pusher_controller.rb
@@ -10,14 +10,14 @@ class Api::PusherController < ApplicationController
     if !@playdate or @playdate.blank?
       render :status=>100, :json=>{:message => "Playdate not found."}
       return
-    elsif p.disconnected?
+    elsif @playdate.disconnected?
       render :status=>101, :json=>{:message=>"Playdate has ended."}
       return
     else
       @playdate.change = Playdate::TURN_PAGE
       @playdate.save
       turnPage
-      render :status=>200, :json=>{:message => 'Turn page sent via pusher'} 
+      render :status=>200, :json=>{:message => 'Turn page sent via pusher on #{@playdate.pusher_channel_name}'} 
     end
   end
 
