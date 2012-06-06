ca18d2d4c897fc5074bd1cdaa890b7152dfc876e
Semira Rahemtulla <semira@gmail.com>
2012-06-05 19:51:25 -0700

    updated html pages for book and pages. moved url formation helper methods from games_helper to application_helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,20 +1,4 @@
 module GamesHelper  
-  def getPageImageFilePath(directory, pageNum) 
-    #for S3 storage, e.g. https://ragatzi.s3.amazonaws.com/little-red-riding-hood-page1.png
-    path = "https://ragatzi.s3.amazonaws.com/" + directory; 
-    if (pageNum.to_i >= 0)
-       path += "-" + "page" + pageNum
-    end
-    path += ".png"
-  end
-  
-  def getImageFilePath(name)
-    "https://ragatzi.s3.amazonaws.com/" + name;
-  end
-  
-  def fellowPlayers()
-    return User.all
-  end
   
 end
 
