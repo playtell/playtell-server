8ac5ca0110e1868a564b1b39618d48ce81070aee
Dimitry <dimitryb@gmail.com>
2012-06-28 10:06:27 -0400

    Sending more data on playdate_ended Pusher

--- app/controllers/api/pusher_controller.rb
+++ app/controllers/api/pusher_controller.rb
@@ -30,11 +30,18 @@ class Api::PusherController < ApplicationController
     playdate.disconnect
     
     # Send notification of playdate-end via rendesvous channel
+    initiator = User.find(playdate.player1_id)
+    playmate = User.find(playdate.player2_id)
     Pusher["presence-rendezvous-channel"].trigger('playdate_ended', {
-      :playdateID        => playdate.id,
-      :pusherChannelName => playdate.pusher_channel_name,
-      :initiatorID       => playdate.player1_id,
-      :playmateName      => playdate.player2_id
+      :playdateID           => playdate.id,
+      :pusherChannelName    => playdate.pusher_channel_name,
+      :initiatorID          => initiator.id,
+      :initiator            => initiator.username,
+      :playmateID           => playmate.id,
+      :playmateName         => playmate.username,
+      :tokboxSessionID      => playdate.video_session_id,
+      :tokboxInitiatorToken => playdate.tokbox_initiator_token,
+      :tokboxPlaymateToken  => playdate.tokbox_playmate_token
     })
   end
 end
