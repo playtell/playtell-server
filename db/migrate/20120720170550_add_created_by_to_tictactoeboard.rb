f0eeb47c340a218bddbe84eab3c7799696dba4a3
Giancarlo Daniele <gdaniele@stanford.edu>
2012-08-01 15:27:29 -0700

    fixed migrations

--- db/migrate/20120720170550_add_created_by_to_tictactoeboard.rb
+++
@@ -1,9 +0,0 @@
-class AddCreatedByToTictactoeboard < ActiveRecord::Migration
-  def self.up
-  	add_column :tictactoeboards, :created_by, :integer
-  end
-
-  def self.down
-  	 add_column :tictactoeboards, :created_by
-  end
-end
