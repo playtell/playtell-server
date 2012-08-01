f13a5b90df6a7a4c8616838a8d7a885d755c2818
Giancarlo Daniele <gdaniele@stanford.edu>
2012-08-01 15:13:23 -0700

    Fixed error in migration

--- db/migrate/20120720163012_fix_naming_tictactoe.rb
+++ db/migrate/20120720163012_fix_naming_tictactoe.rb
@@ -2,14 +2,12 @@ class FixNamingTictactoe < ActiveRecord::Migration
   def self.up
   	remove_column :tictactoeindicators, :board_id
   	add_column :tictactoeindicators, :tictactoeboard_id, :integer
-  	remove_column :tictactoespaces, :board_id
   	add_column :tictactoespaces, :tictactoeboard_id, :integer
   end
 
   def self.down
   	remove_column :tictactoeindicators, :tictactoeboard_id
   	remove_column :tictactoespaces, :tictactoeboard_id
-  	add_column :tictactoespaces, :board_id, :integer
   	add_column :tictactoeindicators, :board_id, :integer
   end
 end
