849b2279f242dcac37efcbdfaa7b378f394237b9
Giancarlo Daniele <gdaniele@stanford.edu>
2012-08-07 22:48:01 -0700

    refactored place_piece

--- app/controllers/api/tictactoe_controller.rb
+++ app/controllers/api/tictactoe_controller.rb
@@ -1,23 +1,26 @@
 class Api::TictactoeController < ApplicationController
 	# ---- CONSTANTS ----
-	# game status
+	# game status codes
 	OPEN_GAME = 0
 	CLOSED_WON = 1
 	CLOSED_CATS = 2
 	CLOSED_UNFINISHED = 3
 
-	# piece placement
+	# piece placement status codes
 	NOT_PLACED = 0
 	PLACED_SUCCESS = 1
 	PLACED_CATS = 2
-	PLACED_WON_ROW_0 = 3
-	PLACED_WON_ROW_1 = 4
-	PLACED_WON_ROW_2 = 5
+	PLACED_WON = 3
+
+	#win status codes
 	PLACED_WON_COL_0 = 6
 	PLACED_WON_COL_1 = 7
 	PLACED_WON_ROW_2 = 8
 	PLACED_WON_ACROSS_TOP_LEFT = 9
 	PLACED_WON_ACROS_BOTTON_LEFT = 10
+	PLACED_WON_ROW_0 = 11
+	PLACED_WON_ROW_1 = 12
+	PLACED_WON_ROW_2 = 13
 
 	 respond_to :json
 	 skip_before_filter :verify_authenticity_token
@@ -25,7 +28,7 @@ class Api::TictactoeController < ApplicationController
 
 	#request params playmate_id
 	def new_game
-		return render :json=>{:message=>"HTTP POST expects \"playmate_id\" and playdate_id and authentication_token as parameters. Refer to the API documentation for more info."} if params[:authentication_token].nil? || params[:playmate_id].nil? || params[:playdate_id].nil?
+		return render :json=>{:message=>"API expects the following: playmate_id, playdate_id, authentication_token. Refer to the API documentation for more info."} if params[:authentication_token].nil? || params[:playmate_id].nil? || params[:playdate_id].nil?
 	
 		# grab the current playdate! 
 		@playdate = Playdate.find_by_id(params[:playdate_id])
@@ -47,9 +50,18 @@ class Api::TictactoeController < ApplicationController
 
 	#request params user_id, board_id, coordinates
 	def place_piece
-		return render :json=>{:message=>"HTTP POST expects parameters \"board_id\", and \"coordinates\"and user_id. Refer to the API documentation for more info."} if params[:user_id].nil? || params[:board_id].nil? || params[:coordinates].nil?
+		##start PARAM validation start
+		return render :json=>{:message=>"API expects the following: board_id, playdate_id, authentication_token, coordinates, and user_id. Refer to the API documentation for more info."} if params[:user_id].nil? || params[:board_id].nil? || params[:coordinates].nil?  || params[:playdate_id].nil? || if params[:authentication_token].nil?
 
-		current_user = User.find_by_id(params[:user_id])
+		return render :json=>{:message=>"Playmate cannot be found."} if current_user.nil?
+
+		#set json response (yes send json w/ response or no)
+		json_response = false
+		if !params[:playdate_id].nil?
+			json_response = true if params[:json_response] == "true"
+
+		@playdate = Playdate.find_by_id(params[:playdate_id])
+		return render :json=>{:message=>"Playdate with id: " + params[:playdate_id] + " not found."} if @playdate.nil?
 
 		return render :json=>{:placement_status => 0, :message=>"Error: Playmate cannot be found."} if current_user.nil? #TODO figure out why json status messages don't work in browser
 
@@ -62,24 +74,37 @@ class Api::TictactoeController < ApplicationController
 		return render :json=>{:placement_status => 0, :message=>"Error: Playmate with id" + current_user.id.to_s() +  "is not authorized to change this board"} if !board.user_authorized(current_user.id)
 		return render :json=>{:placement_status => 0, :message=>"Error: It is not the turn of playmate " + current_user.authentication_token.to_s + " . Try again after opponent makes move."} if !board.is_playmates_turn(current_user.id)
 		return render :json=>{:placement_status => 0, :message=>"Error: Game has already ended or game is invalid"} if board.status != 0
+		
+		##start RESPONSE formation
+		response = {}
+		if json_response
+			board_dump = JSON.dump board
+			spaces_dump = JSON.dump board.tictactoespaces
+			indicators_dump = JSON.dump board.tictactoeindicators
+			status_dump = {:board_dump => board_dump, :spaces_dump => spaces_dump, :indicators_dump => indicators_dump}
+			response.merge(status_dump)
 
+		#default responses
+		response_message = ""
+		placement_status = NOT_PLACED
+
+		#get response
 		response_code = board.mark_location(coordinates, current_user.id)
-
-		#grab board, spaces, and indicators in json format to send to iPads for validation purposes
-		board_dump = JSON.dump board
-		spaces_dump = JSON.dump board.tictactoespaces
-		indicators_dump = JSON.dump board.tictactoeindicators
-
-		# TODO try not to repeat code here (playdate.rb, user.rb have examples of using constants)
 		if  response_code == NOT_PLACED
-			return render :json=>{:placement_status => response_code, :message=>"Error: Piece cannot be placed. Another piece is already at this location", :board_dump => board_dump, :spaces_dump => spaces_dump, :indicators_dump => indicators_dump}
+			response_message = "Placement failed. Another piece at " + params[:coordinates]
 		elsif response_code == PLACED_SUCCESS
-			return render :json=>{:placement_status => response_code, :message=>"Piece successfully placed at " + params[:coordinates], :board_dump => board_dump, :spaces_dump => spaces_dump, :indicators_dump => indicators_dump}
-		elsif response_code == PLACED_WON_ROW_0
-			return render :json=>{:placement_status => response_code, :message=>"Piece successfully placed. " + params[:user_id] + " has won!", :board_dump => board_dump, :spaces_dump => spaces_dump, :indicators_dump => indicators_dump}
+			response_message = "Placement success at " + params[:coordinates]
+		elsif response_code == PLACED_WON
+			response_message = "Placement success at " + params[:coordinates] + ". We have a winner!"
+			win_type = 1 #TODOGIANCARLO fix this board.get_win_status_code()
 		elsif response_code == PLACED_CATS
-			return render :json=>{:placement_status => response_code, :message=>"Piece successfully placed, but it's a cat's game. MEOW", :board_dump => board_dump, :spaces_dump => spaces_dump, :indicators_dump => indicators_dump}
+			response_message = "Placement success at " + params[:coordinates] + ". Cats game!"
 		end
+
+		response_message["placement_code"] = response_code	
+		if response_code == PLACED_WON
+			response_message["win_code"] = win_type
+		render :json => response_message
 	end
 
 	#request params board_id
