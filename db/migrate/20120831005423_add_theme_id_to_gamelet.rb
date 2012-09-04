fa8b9dea95b62d6dcefc963b4c51e663c0d4f4ae
Giancarlo Daniele <gdaniele@stanford.edu>
2012-09-04 16:00:09 -0700

    fixed migrations, again

--- db/migrate/20120831005423_add_theme_id_to_gamelet.rb
+++
@@ -1,9 +0,0 @@
-class AddThemeIdToGamelet < ActiveRecord::Migration
-  def self.up
-  	add_column :gamelets, :theme_id, :integer
-  end
-
-  def self.down
-  	remove_column :gamelets, :theme_id
-  end
-end
