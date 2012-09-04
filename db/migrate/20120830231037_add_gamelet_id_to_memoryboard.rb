1157d6c001b401be9ff93dfbfaa0ccde6bcddf26
Giancarlo Daniele <gdaniele@stanford.edu>
2012-09-04 15:54:09 -0700

    fixed postgres migration problem

--- db/migrate/20120830231037_add_gamelet_id_to_memoryboard.rb
+++
@@ -1,9 +0,0 @@
-class AddGameletIdToMemoryboard < ActiveRecord::Migration
-  def self.up
-  	add_column :memoryboards, :gamelet_id, :integer
-  end
-
-  def self.down
-  	remove_column :memoryboards, :gamelet_id
-  end
-end
