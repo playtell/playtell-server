11455918571d5ebae8f44fedff0d0f02a0cc1e8b
Giancarlo Daniele <gdaniele@stanford.edu>
2012-09-05 13:48:39 -0700

    added score tracking

--- app/models/gamelet.rb
+++ app/models/gamelet.rb
@@ -72,7 +72,7 @@ class Gamelet < ActiveRecord::Base
 		playmate = User.find_by_id(friend_2)
 		return -1 if creator.nil? || playmate.nil?
 
-		board = Memoryboard.create(:win_code => NIL_OR_ERROR, :status => OPEN_GAME, :num_cards_left => total_num_cards, :winner => nil, :initiator_id => creator.id, :playmate_id => playmate.id, :whose_turn => CREATORS_TURN, :gamelet_id => self.id, :num_total_cards => total_num_cards) # it's the creator's turn first
+		board = Memoryboard.create(:initiator_score => 0, :playmate_score => 0, :win_code => NIL_OR_ERROR, :status => OPEN_GAME, :num_cards_left => total_num_cards, :winner => nil, :initiator_id => creator.id, :playmate_id => playmate.id, :whose_turn => CREATORS_TURN, :gamelet_id => self.id, :num_total_cards => total_num_cards) # it's the creator's turn first
 		board.init_card_array
 
 		return board.id
