e2c9b30a066e6430d280c4705c44ff0a8ac68ef4
Dimitry <dimitryb@gmail.com>
2012-09-18 10:00:30 -0400

    Photo upload debug

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'digest/md5'
 
 class PhotoUploader < CarrierWave::Uploader::Base
 
@@ -40,14 +41,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    random_filename = Digest::MD5.hexdigest(rand(1234567).to_s)
+    ext_name = File.extname(super)
+    "#{random_filename}.#{ext_name}" if original_filename
+  end
 
 end
