5464b985977030217fcbaebcc31477163f5c03f9
Semira Rahemtulla <semira@gmail.com>
2012-10-04 17:10:25 -0700

    when creating a new user, friendship with jason is automatically accepted, ie friendship status is set to true right away.

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,9 @@ class User < ActiveRecord::Base
   # auto-adds Test as this user's first friend
   def add_first_friend
     if User.count > 0
-      self.friendships.create!(:friend_id => User.find_by_username(DEFAULT_FRIEND_NAME).id)
+      f = self.friendships.create!(:friend_id => User.find_by_username(DEFAULT_FRIEND_NAME).id)
+      f.status=true;
+      f.save
     end
   end
   
