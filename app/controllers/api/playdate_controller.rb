a534e2a12049a62294c63e959e306c0491274f95
Dimitry <dimitryb@gmail.com>
2012-10-12 16:55:41 -0400

    Disabled Push

--- app/controllers/api/playdate_controller.rb
+++ app/controllers/api/playdate_controller.rb
@@ -57,7 +57,9 @@ class Api::PlaydateController < ApplicationController
          }
        }
        # logger.info("Push notification sent: " + "playdate id: " + @playdate.id.to_s + " device token: " + notification[:device_tokens][0] + " url: " + notification[:aps][:playdate_url] + " initiator: " + notification[:aps][:initiator] + " playmate: " + notification[:aps][:playmate])
-       Urbanairship.push(notification)
+
+       # Temporarity disabled by Dimitry 10/12/12
+       # Urbanairship.push(notification)
      end
      
      # Notify
