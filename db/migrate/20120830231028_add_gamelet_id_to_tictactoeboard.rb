477cba5ede4a48d9c6386e419b40833415561c45
Semira Rahemtulla <semira@gmail.com>
2012-10-29 23:50:40 -0700

    changed migration where gamelet_id gets added to memoryboard - it was added in two separate migrations, so im removing it from addgameletidtotictactoeboard

--- db/migrate/20120830231028_add_gamelet_id_to_tictactoeboard.rb
+++ db/migrate/20120830231028_add_gamelet_id_to_tictactoeboard.rb
@@ -1,12 +1,12 @@
 class AddGameletIdToTictactoeboard < ActiveRecord::Migration
   def self.up
   	add_column :tictactoeboards, :gamelet_id, :integer
-  	add_column :memoryboards, :gamelet_id, :integer
+  	#add_column :memoryboards, :gamelet_id, :integer
 
   end
 
   def self.down
   	remove_column :tictactoeboards, :gamelet_id
-  	remove_column :memoryboards, :gamelet_id
+  	#remove_column :memoryboards, :gamelet_id
   end
 end
