3c545ef91bd49f211f45afe46539dc24163d4416
Dimitry <dimitryb@gmail.com>
2012-12-11 18:19:13 -0500

    Debug: Testing high version number

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 DEFAULT_FRIEND_NAME = "jason" #username
 S3_BUCKET_NAME = "http://playtell.s3.amazonaws.com"
-CURRENT_MAJOR_VERSION = "1.0"
+CURRENT_MAJOR_VERSION = "7.0"
 APP_STORE_LINK = 'http://itunes.apple.com/us/app/id570304295'
