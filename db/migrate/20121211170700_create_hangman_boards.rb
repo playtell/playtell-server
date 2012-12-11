4c7307973d637bec4ac4fa3bd02454ef9ae7384a
Dimitry <dimitryb@gmail.com>
2012-12-11 17:26:29 -0500

    Typo fix

--- db/migrate/20121211170700_create_hangman_boards.rb
+++ db/migrate/20121211170700_create_hangman_boards.rb
@@ -1,14 +1,14 @@
 class CreateHangmanBoards < ActiveRecord::Migration
   def self.up
     create_table :hangman_boards do |t|
-      t.integer :state,
-      t.integer :playdate_id,
-      t.integer :initiator_id,
-      t.integer :playmate_id,
+      t.integer :state
+      t.integer :playdate_id
+      t.integer :initiator_id
+      t.integer :playmate_id
       t.integer :misses, :default => 0
       t.integer :whose_turn, :default => 0
-      t.integer :winner,
-      t.string :word,
+      t.integer :winner
+      t.string :word
       t.string :word_bits
 
       t.timestamps
