f25b7644280f167c86be7d3d49656f4b0cd73c5b
Semira Rahemtulla <semira@gmail.com>
2013-01-21 13:00:10 -0800

    changed actvities list api to return whether an activity is a book, and if so, include the book_id

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,8 +2,12 @@ class Activity < ActiveRecord::Base
   has_one :book, :dependent => :destroy
 
   def as_json(options={})
-    { :id => self.id,
-      :title => self.title }
+    is_book = !self.book.nil?
+    a = { :id => self.id,
+      :title => self.title,
+      :is_book => is_book }
+    a.merge(:book_id => self.book.id) unless !is_book
+    return a
   end
 
 end
