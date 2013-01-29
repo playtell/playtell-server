6624c5043ded860f4475051532a1c06b09700e30
Semira Rahemtulla <semira@gmail.com>
2013-01-29 15:44:47 -0800

    added page images to book view when book is image-only.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+
+#depricated
   def getPageImageFilePath(directory, pageNum) 
     #for S3 storage, e.g. https://ragatzi.s3.amazonaws.com/little-red-riding-hood-page1.png
     path = "https://ragatzi.s3.amazonaws.com/" + directory; 
@@ -7,9 +9,14 @@ module ApplicationHelper
     end
     path += ".png"
   end
-  
+
+#depricated
   def getImageFilePath(name)
     "https://ragatzi.s3.amazonaws.com/" + name;
+  end
+  
+  def getPage(directory, pageNum)
+    "#{S3_BUCKET_NAME}/books/#{directory}/page#{pageNum}"
   end
   
   def fellowPlayers()
