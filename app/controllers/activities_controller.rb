0bc5fcccb98939c710298ba207b65a4a6ece7059
Semira Rahemtulla <semira@gmail.com>
2013-01-29 16:30:03 -0800

    removed toybox for user#show so that changes to books and games dont affect the rest of the site. added pageimage which shows all the page images for image-only books. fixed a bug wherein a new book always got created immediately upon clicking Create a New Book, rather than on Save Book.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,10 +12,10 @@ class ActivitiesController < ApplicationController
   def newactivity
     @activity = Activity.new
     if params[:activity_type] == "book" 
-      @activity.create_book(:image_only => 1)
+      @activity.build_book(:image_only => 1)
       render 'newbook'
     else
-      @activity.create_game
+      @activity.build_game
       render 'newgame'
     end   
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,21 +2,16 @@ module ApplicationHelper
 
 #depricated
   def getPageImageFilePath(directory, pageNum) 
-    #for S3 storage, e.g. https://ragatzi.s3.amazonaws.com/little-red-riding-hood-page1.png
-    path = "https://ragatzi.s3.amazonaws.com/" + directory; 
-    if (pageNum.to_i >= 0)
-       path += "-" + "page" + pageNum
+    if (pageNum.to_i > 0)
+      "#{S3_BUCKET_NAME}/books/#{directory}/page#{pageNum}"
+    elsif (pageNum.to_i == 0)
+      "#{S3_BUCKET_NAME}/books/#{directory}/cover_front"
     end
-    path += ".png"
   end
 
 #depricated
   def getImageFilePath(name)
     "https://ragatzi.s3.amazonaws.com/" + name;
-  end
-  
-  def getPage(directory, pageNum)
-    "#{S3_BUCKET_NAME}/books/#{directory}/page#{pageNum}"
   end
   
   def fellowPlayers()
