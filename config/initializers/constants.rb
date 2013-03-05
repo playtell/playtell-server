dea3d00c94a2896c1e63fc75144d8d7d2b4ae361
Semira Rahemtulla <semira@gmail.com>
2013-03-05 11:42:11 -0800

    updating server version for new app version with webrtc

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 DEFAULT_FRIEND_NAME = "jason" #username
 S3_BUCKET_NAME = "http://playtell.s3.amazonaws.com"
-CURRENT_MAJOR_VERSION = "3.0"
+CURRENT_MAJOR_VERSION = "5.0"
 APP_STORE_LINK = 'http://itunes.apple.com/us/app/id570304295'
