f8ff4956dd4498fcae2fb0ac4b30da74f785d393
Semira Rahemtulla <semira@gmail.com>
2013-03-18 09:45:32 -0700

    set default host for mailer in production. currently set to staging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,9 @@ Semiralabs::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  # Raise errors if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'playtell-staging.heroku.com' }
 
   # Enable threaded mode
   # config.threadsafe!
